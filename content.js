720b7502a004d63fa5fc344bf294cdee27f6e934
echanlee <40003439+echanlee@users.noreply.github.com>
2019-07-05 19:57:48 -0400

    Update content.js

--- content.js
+++ content.js
@@ -6,7 +6,6 @@ function myFunction() {
     var inputMessage = document.querySelector("textarea.docos-input-textarea");
     inputMessage.addEventListener("keyup", function(e) {
         if(e.keyCode == 190){
-              console.log("HELLO");
               inputMessage.value = inputMessage.value +" :)";
         }
   })
